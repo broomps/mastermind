2699335a1055d7e205dbf437e29f4b1bac5483e4
broomps <69511971+broomps@users.noreply.github.com>
2024-01-03 13:02:39 +0000

    Made it so you can change the colour of buttons Everytime a button is clicked it will change the colour - colour checking to be added in the next commit.

--- button.py
+++ button.py
@@ -1,15 +1,27 @@
 class Button:
     def __init__(self, pygame, white=(255,255,255), black=(0,0,0), green =(152,229,165), red=(172,56,52), orange=(252,138,23), blue = (43,98,244), yellow=(249,224,118), brown=(154,123,79)):
-        self.white = white
-        self.black = black
-        self.green = green
-        self.red = red
-        self.orange = orange
-        self.blue = blue
-        self.yellow = yellow
-        self.brown = brown
+        self.colours = (white, black, green, red, orange, blue, yellow , brown)
         self.pygame = pygame
+        self.colour = 0
+        self.clicked = False
 
-    def draw(self, screen, cell_size, x, y):
-        button1_rect = self.pygame.Rect(x * cell_size, y * cell_size, cell_size * 4, cell_size * 4)
-        self.pygame.draw.rect(screen, self.green, button1_rect)
+    def initial_draw(self, screen, cell_size, x, y):
+        button_rect = self.pygame.Rect(x * cell_size, y * cell_size, cell_size * 4, cell_size * 4)
+        self.pygame.draw.rect(screen, self.colours[self.colour], button_rect)
+
+
+    def check_clicked(self, screen, cell_size, x, y):
+        #get mouse position
+        pos = self.pygame.mouse.get_pos()
+        #Creates the button rectangle
+        button_rect = self.pygame.Rect(x * cell_size, y * cell_size, cell_size * 4, cell_size * 4)
+
+        #check mouseover and clicked conditions
+        if button_rect.collidepoint(pos):
+            if self.pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.colour += 1
+                self.pygame.draw.rect(screen, self.colours[self.colour%len(self.colours)], button_rect)
+        
+        if self.pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
--- main.py
+++ main.py
@@ -16,6 +16,19 @@ clock = pygame.time.Clock()
 
 board = Board(Vector2, pygame)
 
+screen.fill(board.BROWN)
+board.draw(screen, cell_size, Vector2(4, 1))
+
+button1 = Button(pygame)
+button2 = Button(pygame)
+button3 = Button(pygame)
+button4 = Button(pygame)
+
+button1.initial_draw(screen, cell_size, 15, 3)
+button2.initial_draw(screen, cell_size, 23, 3)
+button3.initial_draw(screen, cell_size, 31, 3)
+button4.initial_draw(screen, cell_size, 39, 3)
+
 while True:
     #Checks if the x to close button is clicked
     for event in pygame.event.get():
@@ -23,13 +36,11 @@ while True:
             pygame.quit()
             sys.exit("Thank you for playing!")
     
-    #Drawing
-    screen.fill(board.BROWN)
-    board.draw(screen, cell_size, Vector2(4, 1))
-
-    button1 = Button(pygame)
-    for i in range(15, 40, 8):
-        button1.draw(screen, cell_size, i, 3)
+    #Checking for collisions
+    button1.check_clicked(screen, cell_size, 15, 3)
+    button2.check_clicked(screen, cell_size, 23, 3)
+    button3.check_clicked(screen, cell_size, 31, 3)
+    button4.check_clicked(screen, cell_size, 39, 3)
 
     #Game clock
     pygame.display.update()
