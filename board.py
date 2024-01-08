d69b7f16a31a0e43b52baba8b750c0050d83fe33
broomps <69511971+broomps@users.noreply.github.com>
2024-01-08 10:44:50 +0000

    Finished commenting

--- board.py
+++ board.py
@@ -17,16 +17,22 @@ class Board:
         self.pygame.draw.rect(screen, self.DARKEST_BROWN, line_rect)
     
     def win(self, screen, cell_size, font, guesses):
+        #Screen is filled with green
         screen.fill((0, 179, 138))
 
+        #If they win in one guess it says guess instead of guesses
+        #It creates a rendered image(?) with the text in a specific font and colour
         if guesses == 1:
             text_img = font.render(f"YOU WIN IN 1 GUESS", True, (255, 255, 255))
         else:
             text_img = font.render(f"YOU WIN IN {guesses} GUESSES", True, (255, 255, 255))
+        #It blits the text onto the screen
         screen.blit(text_img, (100, 200))
     
     def lose(self, screen, cell_size, font):
+        #It fills the screen with red
         screen.fill((234, 50, 74))
 
+        #You lose is displayed on the screen
         text_img = font.render("YOU LOSE", True, (255, 255, 255))
         screen.blit(text_img, (200, 200))
--- main.py
+++ main.py
@@ -5,31 +5,38 @@ from button import *
 from textbutton import *
 from keypeg import *
 
+#Initializes all of the variables used in the program
+won = False
+row = 0
+end = False
+x = 7
+y = 1
+cell_size = 10
+number_of_cells = 50
 buttons = [0, 0, 0, 0]
 answer = []
+#The answer is randomly generated
 for i in range(4):
     answer.append(random.randint(0, 5))
 
+#Pygame initialization
 pygame.init()
 
-cell_size = 10
-number_of_cells = 50
-
+#Screen is set up with the mastermind caption
 screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
 pygame.display.set_caption("Mastermind")
 
-font = pygame.font.Font('retro-grade-2-font/RetroGradeItalic-2OZYv.otf', 20)
-
+#FPS is initialized
 clock = pygame.time.Clock()
 
+#The board instance of class Board is initialized
 board = Board(Vector2, pygame)
 
-x = 7
-y = 1
-
+#Draws the initial board
 screen.fill(board.BROWN)
 board.draw(screen, cell_size, Vector2(x, y))
 
+#Initializes all of the buttons and draws them at their respective coordinates
 button1 = Button(pygame)
 button2 = Button(pygame)
 button3 = Button(pygame)
@@ -40,16 +47,14 @@ button2.initial_draw(screen, cell_size, x + 19, y + 2)
 button3.initial_draw(screen, cell_size, x + 27, y + 2)
 button4.initial_draw(screen, cell_size, x + 35, y + 2)
 
+#The guess button is initialized and drawn on the screen
 guess_button = Text_Button(pygame, "Guess")
-guess_button.draw(screen, cell_size, 0, 0, font)
+guess_button.draw(screen, cell_size, 0, 0, pygame.font.Font('retro-grade-2-font/RetroGradeItalic-2OZYv.otf', 20))
 
-won = False
-
-row = 0
-end = False
-
+#Y values for the pegs is set up
 y_values = [(y + 2), (y + 7), (y + 12), (y + 17), (y + 22), (y + 27), (y + 32), (y + 37), (y + 42)]
 
+#Game loop
 while True:
     #Checks if the x to close button is clicked
     for event in pygame.event.get():
@@ -63,38 +68,48 @@ while True:
     buttons[2] = button3.check_clicked(screen, cell_size, x + 27, y_values[row])
     buttons[3] = button4.check_clicked(screen, cell_size, x + 35, y_values[row])
 
+    #Checks if the guess button has been clicked
     guessed = guess_button.check_click(screen, cell_size, 0, 0)
     if guessed == True:
+        #Creates a list with all all the values for if they are correct
         correct = guess_button.check_correct(answer, buttons)
+        #Initializes all of the key pegs
         peg1 = Key_Peg(pygame)
         peg2 = Key_Peg(pygame)
         peg3 = Key_Peg(pygame)
         peg4 = Key_Peg(pygame)
+        #Places all of the key pegs with the correct colour
         peg1.place(screen, cell_size, 9, y_values[row], correct[0])
         peg2.place(screen, cell_size, 11, y_values[row], correct[1])
         peg3.place(screen, cell_size, 9, y_values[row] + 2, correct[2])
         peg4.place(screen, cell_size, 11, y_values[row] + 2, correct[3])
 
+        #Checks if they have won
         won = guess_button.check_win(correct)
 
+        #If they have won
         if won == True:
+            #The winning screen is displayed
             board.win(screen, cell_size, pygame.font.Font('retro-grade-2-font/RetroGradeItalic-2OZYv.otf', 30), row + 1)
         else:
+            #Row + 1 means go to the next row down
             row += 1
             try:
+                #Try to draw another row of buttons
                 button1.initial_draw(screen, cell_size, x + 11, y_values[row])
                 button2.initial_draw(screen, cell_size, x + 19, y_values[row])
                 button3.initial_draw(screen, cell_size, x + 27, y_values[row])
                 button4.initial_draw(screen, cell_size, x + 35, y_values[row])
             except IndexError:
+                #If they have made the maximum number of guesses they lose
                 board.lose(screen, cell_size, pygame.font.Font('retro-grade-2-font/RetroGradeItalic-2OZYv.otf', 50))
-                print(row)
                 end = True
 
     #Game clock
     pygame.display.update()
     clock.tick(60)
 
+    #If they have lost the game waits 5 seconds then closes
     if end == True:
         time.sleep(5)
         pygame.quit()
